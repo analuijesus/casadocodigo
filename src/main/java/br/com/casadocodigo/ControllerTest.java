3e191c6db82fc59fb84c52a84d19104dd0f33eec
Ana de Jesus <anadejesus.dev@gmail.com>
2022-08-10 19:42:51 -0300

    iniciando o projeto e implementando swagger

---
+++ src/main/java/br/com/casadocodigo/ControllerTest.java
@@ -0,0 +1,13 @@
+package br.com.casadocodigo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerTest {
+
+    @GetMapping(value = "/api/test")
+    public String test() {
+        return "Funcionou!!";
+    }
+}
