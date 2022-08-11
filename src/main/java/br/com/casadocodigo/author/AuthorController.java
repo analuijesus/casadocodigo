96384665439319f0bcf221e29ccecb8f4153818c
Ana de Jesus <anadejesus.dev@gmail.com>
2022-08-11 16:44:47 -0300

    criando repository para autor e adicionando a obrigatoriedade da transactional

--- src/main/java/br/com/casadocodigo/author/AuthorController.java
+++ src/main/java/br/com/casadocodigo/author/AuthorController.java
@@ -1,24 +1,21 @@
 package br.com.casadocodigo.author;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 @RestController
 public class AuthorController {
-
-    @PersistenceContext
-    private EntityManager entityManager;
+    @Autowired
+    private AuthorRepository authorRepository;
 
     @PostMapping(value = "api/author")
     @Transactional
     public void newAuthor(@Valid @RequestBody NewAuthorForm newAuthorForm) {
         Author newAuthor = newAuthorForm.newAuthor();
-        entityManager.persist(newAuthor);
+        authorRepository.save(newAuthor);
     }
 }
---
+++ src/main/java/br/com/casadocodigo/author/AuthorRepository.java
@@ -0,0 +1,6 @@
+package br.com.casadocodigo.author;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+}
